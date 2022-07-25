712a16c7e6afef16c0cac241fb857e2b8f97033a
abdo2000320 <abdellahelgourami123@gmail.com>
2022-07-25 16:35:04 +0100

    Find the duplicate Number

--- Arrays/Find the Duplicate Number.java
+++ Arrays/Find the Duplicate Number.java
@@ -1,11 +1,11 @@
-
+//Find duplicate Numbers in an array
 class Solution {
     public int findDuplicate(int[] nums) {
         int len = nums.length;
-        int[] count = new int[len+1];
+        int[] counter = new int[len+1];
         for(int i=0;i<len;i++){
-            count[nums[i]]++;
-            if(count[nums[i]]>1)
+            counter[nums[i]]++;
+            if(counter[nums[i]]>1)
                 return nums[i];
         }
         
