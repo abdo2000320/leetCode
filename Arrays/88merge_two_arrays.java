558ad3fa6a23b931841c177b9f5e1d03b84c1346
abdo2000320 <abdellahelgourami123@gmail.com>
2022-10-08 09:48:18 +0100

    merge two sorted array

---
+++ Arrays/88merge_two_arrays.java
@@ -0,0 +1,13 @@
+class Solution {
+   public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int end1 = m - 1, end2 = n - 1, finished = m + n - 1;
+        while (end1 >= 0 && end2 >= 0) {
+            nums1[finished--] = (nums1[end1] > nums2[end2]) ? 
+                                nums1[end1--] : nums2[end2--];
+        }
+
+        while (end2 >= 0) { //if nums2 contains yet elements not copied to nums1 
+            nums1[finished--] = nums2[end2--];
+        }
+    }
+}
